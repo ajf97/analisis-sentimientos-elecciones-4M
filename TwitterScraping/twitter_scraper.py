6f00dab249fe957925e134657012c2860288bca0
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2021-05-11 12:47:50 +0200

    refactor: :art: code refactoring with new functions
    This commit introduces new functions for setup and tweets search from topics.

--- TwitterScraping/twitter_scraper.py
+++ TwitterScraping/twitter_scraper.py
@@ -2,32 +2,46 @@ __author__ = "Alejandro Jerónimo"
 
 # Import libraries
 
+from credentials import *
 import tweepy as tw
 import pandas as pd
 import json
 
-consumer_key = "tn5h2e8ZQCZstJWChmvgD1EVO"
-consumer_secret = "6tevkUQXL1t2LvHjKv7mofK3SCo1wHJhSKI2VYeXEqB97d0T2z"
-access_token = "400222622-Hgk5MrTBrXNuz4SAFy6kNDychrxXQv0wljx2vy2D"
-access_token_secret = "16uwgKrGHzwJv8jsyfn7GNAM0xDzW5qOcvXSfM3Ci7Ttq"
 
-# API authentication
+# API setup
 
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def setup():
 
-api = tw.API(auth, wait_on_rate_limit=True,
- wait_on_rate_limit_notify=True)
+    """
+    Setup the Twitter's API with the keys provided in credentials.py
+    """
+
+    # Authentication
+    auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tw.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+    return api
+
+
+def search_topic(api, topic, items, date_from=None, date_to=None):
+    # Collect tweets
+    tweets = tw.Cursor(api.search_30_day,
+    environment_name='dev',
+    query=topic,
+    fromDate=date_from,
+    toDate=date_to).items(items)
+
+    return tweets
 
 
 # Define the topic search and date variables
-search_topic = "#Elecciones4M"
+topic = "#Elecciones4M"
 date_from = "202105040900"
 date_to = "202105042300"
+n_items = 5
 
-# Collect tweets
-tweets = tw.Cursor(api.search_30_day,
-environment_name='dev',
- query=search_topic,
-fromDate=date_from,
-toDate=date_to).items(5)
+api = setup()
+tweets = search_topic(api, topic, n_items, date_from, date_to)
