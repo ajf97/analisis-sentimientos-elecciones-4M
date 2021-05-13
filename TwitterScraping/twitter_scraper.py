64f17280b2cafd266cd659a7a551240bc43f469d
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2021-05-13 11:56:51 +0200

    style: :art: style guide applied

--- TwitterScraping/twitter_scraper.py
+++ TwitterScraping/twitter_scraper.py
@@ -1,43 +1,40 @@
 __author__ = "Alejandro Jerónimo"
 
-# Import libraries
+# %% Import libraries
 
-from credentials import *
+import credentials as cr
 import tweepy as tw
-import pandas as pd
-import json
 
+# %% API setup
 
-# API setup
 
 def setup():
-
     """
     Setup the Twitter's API with the keys provided in credentials.py
     """
 
     # Authentication
-    auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = tw.OAuthHandler(cr.CONSUMER_KEY, cr.CONSUMER_SECRET)
+    auth.set_access_token(cr.ACCESS_TOKEN, cr.ACCESS_TOKEN_SECRET)
 
-    api = tw.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
-
+    api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
 
 
 def search_topic(api, topic, items, date_from=None, date_to=None):
     # Collect tweets
-    tweets = tw.Cursor(api.search_30_day,
-    environment_name='dev',
-    query=topic,
-    fromDate=date_from,
-    toDate=date_to).items(items)
+    tweets = tw.Cursor(
+        api.search_30_day,
+        environment_name="dev",
+        query=topic,
+        fromDate=date_from,
+        toDate=date_to,
+    ).items(items)
 
     return tweets
 
 
-# Define the topic search and date variables
+# %% Define the topic search and date variables
 topic = "#Elecciones4M"
 date_from = "202105040900"
 date_to = "202105042300"
