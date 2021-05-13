81775ec5a3d1afdaf9aa3c1ac03ac2f95aa579c3
Alejandro Jerónimo Fuentes <ajf97@correo.ugr.es>
2021-05-13 13:08:10 +0200

    chore: created dataframe with relevant information of tweets

--- TwitterScraping/twitter_scraper.py
+++ TwitterScraping/twitter_scraper.py
@@ -4,6 +4,8 @@ __author__ = "Alejandro Jerónimo"
 
 import credentials as cr
 import tweepy as tw
+import pandas as pd
+import numpy as np
 
 # %% API setup
 
@@ -42,3 +44,15 @@ n_items = 5
 
 api = setup()
 tweets = search_topic(api, topic, n_items, date_from, date_to)
+tweets = [tweet for tweet in tweets]
+# %% Create dataframe
+
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=["Tweets"])
+
+
+data['len'] = np.array([len(tweet.text) for tweet in tweets])
+data['ID'] = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes'] = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs'] = np.array([tweet.retweet_count for tweet in tweets])
